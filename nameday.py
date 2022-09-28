21be3a1a86d1266291d5a60273532ebd7fc8e6ba
DavidRadd <DavidRadd>
2022-09-28 20:46:28 +0000

    mixed lists

---
+++ nameday.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+print("Greetings user!!!")
+
+name = input("Please enter your name: ")
+dow = input("What day is today? ")
+print("\n\nRumor has it that you are %s.\nI've also heard that today is %s." % (name, dow))
+
+print("\nHello %s! Happy %s!" %(name,dow))
