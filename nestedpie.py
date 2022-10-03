c420759f2d3e079d29df79850e4c0ec8ef9fd2dc
DavidRadd <DavidRadd>
2022-10-03 19:30:23 +0000

    Nested Pie test

--- nestedpie.py
+++ nestedpie.py
@@ -10,7 +10,7 @@ size = 0.3
 vals = np.array([[60., 32.], [37., 40.], [29., 10.]])
 
 cmap = plt.colormaps["tab20c"]
-outer_colors = cmap(np.arange(3)*4)
+outer_colors = cmap(np.arange(4)*4)
 inner_colors = cmap([1, 2, 5, 6, 9, 10])
 
 ax.pie(vals.sum(axis=1), radius=1, colors=outer_colors,
