c8b33591256280fc302223de62c1f5cf6fcc3447
DavidRadd <DavidRadd>
2022-10-05 21:08:39 +0000

    csv and pandas

---
+++ mygame01.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+"""Driving a simple game framework with
+   a dictionary object | Alta3 Research"""
+
+def showInstructions():
+    """Show the game instructions when called"""
+    #print a main menu and the commands
+    print('''
+    RPG Game
+    ========
+    Commands:
+      go [direction]
+      get [item]
+    ''')
+
+def showStatus():
+    """determine the current status of the player"""
+    # print the player's current location
+    print('---------------------------')
+    print('You are in the ' + currentRoom)
+    # print what the player is carrying
+    print('Inventory:', inventory)
+    # check if there's an item in the room, if so print it
+    if "item" in rooms[currentRoom]:
+      print('You see a ' + rooms[currentRoom]['item'])
+    print("---------------------------")
+
+
+# an inventory, which is initially empty
+inventory = []
+
+# a dictionary linking a room to other rooms
+## A dictionary linking a room to other rooms
+## A dictionary linking a room to other rooms
+rooms = {
+
+            'Hall' : {
+                  'south' : 'Kitchen',
+                  'east'  : 'Dining Room',
+                  'item'  : 'key'
+                },
+
+            'Kitchen' : {
+                  'north' : 'Hall',
+                  'item'  : 'monster',
+                  'loot'  : 'delicious chocolate chip cookie'
+                },
+            'Dining Room' : {
+                  'west' : 'Hall',
+                  'south': 'Garden',
+                  'item' : 'potion'
+               },
+            'Garden' : {
+                  'north' : 'Dining Room',
+                  'item'  : 'sword'
+            }
+         }
+
+
+
+# start the player in the Hall
+currentRoom = 'Hall'
+
+showInstructions()
+
+# breaking this while loop means the game is over
+while True:
+    showStatus()
+
+    # the player MUST type something in
+    # otherwise input will keep asking
+    move = ''
+    while move == '':  
+        move = input('>')
+
+    # normalizing input:
+    # .lower() makes it lower case, .split() turns it to a list
+    # therefore, "get golden key" becomes ["get", "golden key"]          
+    move = move.lower().split(" ", 1)
+
+    #if they type 'go' first
+    if move[0] == 'go':
+        #check that they are allowed wherever they want to go
+        if move[1] in rooms[currentRoom]:
+            #set the current room to the new room
+            currentRoom = rooms[currentRoom][move[1]]
+        # if they aren't allowed to go that way:
+        else:
+            print('You can\'t go that way!')
+
+    #if they type 'get' first
+    if move[0] == 'get' :
+        # make two checks:
+        # 1. if the current room contains an item
+        # 2. if the item in the room matches the item the player wishes to get
+        if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+            #add the item to their inventory
+            inventory.append(move[1])
+            #display a helpful message
+            print(move[1] + ' got!')
+            #delete the item key:value pair from the room's dictionary
+            del rooms[currentRoom]['item']
+        # if there's no item in the room or the item doesn't match
+        else:
+            #tell them they can't get it
+            print('Can\'t get ' + move[1] + '!')
+
+    ## If a player enters a room with a monster
+    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'sword' not in inventory:
+        print('A monster has got you... GAME OVER!')
+        break
+    elif 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'sword' in inventory:
+        print("A fuzzy blue creature of small stature leans over the kitchen island. 'Nom nom nom' he says as he snorts and grunts and shovels cookies into his drooling maw.")  
+        print("He stops eating to stare at you. Bobbly eyes flaring red.")
+        print("Me Cookie! he screams.")
+        print("\nGibbets of spits and cookies spowe from his mouth.")
+        print("\nYou raise your newly found sword and leap forward. He raises his arms as your sword batters him.")
+        print("Leaving deep oozing crimson gashes on his forearms.") 
+        print("Aaaaaagh! he howls. Leave me alone! Gibbets of spit and cookies spew from his mouth.")
+        print("You swing mightily and severe his soft squeezable fuzzy head from his cute body.") 
+    
+        print("You notice that there remains on the count one delicious chocolate chip cooke.")
+        del rooms[currentRoom]['item']
+        
+        rooms[currentRoom]['item'] = 'a delicious chocolate chip cookie'
+        print('You notice that on the island there remains one delicious looking chocolate chip cookie.')
+        break
+    
+    ## Define how a player can win
+    if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
+        print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
+        break
+    
+
